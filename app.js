530ef5183e6b171c9aafc75ed3a8c7130173ad94
Jessica <jesspeters143@gmail.com>
2022-01-12 20:15:46 -0800

    fist commit

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 // Person constructor
-function Person(name,dob) {
-    this.name = name;
-    // this.age = age;
+function Person(firstName,lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.birthday = new Date(dob);
-    this.calculateAge = function(){
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
+    // this.calculateAge = function(){
+    //     const diff = Date.now() - this.birthday.getTime();
+    //     const ageDate = new Date(diff);
+    //     return Math.abs(ageDate.getUTCFullYear() - 1970);
+    // }
 }
 
-// const brad = new Person('Brad', 36);
-// const john = new Person('John', 30);
+// Calculate age
+Person.prototype.calculateAge = function(){
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+const brad = new Person('Brad', 'Pitt', '8-12-90');
+const mary = new Person('Mary', 'White', 'March 23 1978');
+
+console.log(mary);
+
+console.log(brad.calculateAge());
+
 
 // console.log(Brad.age);
 
-const brad = new Person('Brad', '9-8-1981');
-console.log(brad.calculateAge());
+// const brad = new Person('Brad', '9-8-1981');
+// console.log(brad.calculateAge());
+
+// Class takes the function and simplify. Every method added inside the class stays on the prototype
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
